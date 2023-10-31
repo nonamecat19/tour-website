8e60f49bbbe785b14e620bb5b6b5b1df1f5ff1f9
Shumsky Alexander <aleksandr.shumskyi@4k.com.ua>
2023-10-31 22:34:52 +0200

    frontend init

---
+++ nextjs-frontend/app/layout.tsx
@@ -0,0 +1,23 @@
+import type {Metadata} from 'next'
+import {Inter} from 'next/font/google'
+import '@/styles/global.css'
+import {ReactNode} from "react";
+
+const inter = Inter({subsets: ['latin']})
+
+export const metadata: Metadata = {
+  title: 'Tour website',
+  description: 'Tour website description',
+}
+
+interface IProps {
+  children: ReactNode
+}
+
+export default function RootLayout({children}: IProps) {
+  return (
+    <html lang="en">
+    <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
---
+++ nextjs-frontend/app/page.tsx
@@ -0,0 +1,11 @@
+import BaseHeader from "@/components/BaseHeader";
+import TourList from "@/components/TourList";
+
+export default function Home() {
+  return (
+    <main>
+      {/*<BaseHeader/>*/}
+      <TourList/>
+    </main>
+  )
+}
